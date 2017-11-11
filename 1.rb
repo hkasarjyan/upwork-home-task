d77a19c917dd8404a725dd16b87c5a58726bb194
Hovhannes Kasarjyan <hovhannes.kasarjyan@emsisoft.com>
2017-11-11 11:55:16 +0400

    feat: added first line of code for usning selenium

--- 1.rb
+++ 1.rb
@@ -1,1 +1,8 @@
-puts 'Hello, world!'
+driver = Selenium::WebDriver.for :firefox
+driver.navigate.to "http://google.com"
+
+element = driver.find_element(:name, 'q')
+element.send_keys "Selenium Tutorials"
+element.submit
+
+driver.quit
