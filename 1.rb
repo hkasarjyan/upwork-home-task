7b845f20ac300b4fd77c717fa79e03e9d8f1a843
Hovhannes Kasarjyan <hovhannes.kasarjyan@emsisoft.com>
2017-11-11 18:32:14 +0400

    feat: first version of simle senelnium case

--- 1.rb
+++ 1.rb
@@ -1,8 +1,27 @@
-driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "http://google.com"
-
-element = driver.find_element(:name, 'q')
-element.send_keys "Selenium Tutorials"
-element.submit
-
-driver.quit
+require "selenium-webdriver"
+require "test/unit"
+ 
+class LoginClass < Test::Unit::TestCase
+ 
+  def setup
+    Selenium::WebDriver::Chrome.driver_path = File.join(File.absolute_path('', File.dirname("C://Projects/Selenuim/Drivers")),"Drivers","chromedriver.exe")
+    @driver = Selenium::WebDriver.for :chrome
+    @driver.get('http://blog.yoniflenner.net/demo')
+    @driver.manage.window.maximize    
+  end
+ 
+ 
+  def teardown
+    @driver.quit
+  end
+ 
+ 
+  def test_login
+    @driver.find_element(:name, "username").send_keys "admin"
+    @driver.find_element(:name, "password").send_keys "demo"
+    @driver.find_element(:id, "submit").click
+    sleep 0.3
+    assert(@driver.find_element(:id => "loggedin").text.include?("You Are Logged in"),"Assertion Failed")
+    @driver.find_element(:id, "logout").click
+  end
+end
